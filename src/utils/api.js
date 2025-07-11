329a5199f7765a44278c7bd9d3d36b9ab1fb3f86
ThamiraWeerakoon <thamira.tr@gmail.com>
2025-07-11 18:49:40 +0530

    Updated api.

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,9 +1,14 @@
 import axios from 'axios';
 
 // Axios instance configured with base URL and token from localStorage
+//const api = axios.create({
+//    baseURL: 'http://localhost:5050/api',
+//    withCredentials: true, // Send cookies/token across origin
+//});
+
 const api = axios.create({
-    baseURL: 'http://localhost:5050/api',
-    withCredentials: true, // Send cookies/token across origin
+    baseURL: process.env.REACT_APP_API_URL,
+    withCredentials: true,
 });
 
 api.interceptors.request.use((config) => {
